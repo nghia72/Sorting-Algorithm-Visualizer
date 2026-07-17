925895a73c87b1c93949a3822cd88676861924d3
nghia72 <lt.thu280879@gmail.com>
2026-07-17 13:48:27 +0700

    nội dung thay đổi ở đây

--- src/algorithms/bubble.py
+++ src/algorithms/bubble.py
@@ -8,3 +8,9 @@ def bubble_sort(arr):
                 yield arr, j, j + 1, "swap"
     yield arr, -1, -1, "done"
 
+
+if __name__ == "__main__":
+    numbers = [5, 1, 4, 2, 8]
+    list(bubble_sort(numbers))
+    print("Sorted array:", numbers)
+
